3574c07a8e78f302c021bd2f3e4f860354a39338
ncliff-git <moon.light.2000az@gmail.com>
2022-02-25 23:25:36 +0300

    rm all the files

--- app/src/main/java/com/github/ncliff/passet/model/boxes/entities/Box.kt
+++
@@ -1,8 +0,0 @@
-package com.github.ncliff.passet.model.boxes.entities
-
-data class Box(
-    val id: Long,
-    val name: String,
-    val login: String,
-    val password: String,
-)
--- app/src/main/java/com/github/ncliff/passet/model/boxes/entities/BoxCreateData.kt
+++
@@ -1,8 +0,0 @@
-package com.github.ncliff.passet.model.boxes.entities
-
-data class BoxCreateData(
-    val name: String,
-    val login: String,
-    val password: String,
-    val repeatPassword: String,
-)
